02c5c70d64f31840a487edd030e5354bd2f87dfd
Rafael Porto <rafaelppporto2@gmail.com>
2025-02-07 03:13:26 -0300

    teste

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -37,6 +37,7 @@ public class Principal {
 
         for (int i = 0; i < dados.totalTemporadas(); i++) {
             //List<DadosTemporada> episodiosTemporada = temporadas.get(i).episodios();
+            
         }
     }
 }
